e8470abdf2f09d8e5160c033248cfd9a50332bf3
Abel <abelvanhulst@gmail.com>
2023-01-20 21:09:36 +0100

    idk

--- app/woorden/[id]/page.tsx
+++ app/woorden/[id]/page.tsx
@@ -9,9 +9,9 @@ export const dynamic = 'auto',
     preferredRegion = 'auto'
 
 const db = new PocketBase('https://minerva-online.pockethost.io');
+db.admins.authWithPassword('abelvanhulst@gmail.com', '7g#Z5iVbQ&yTd8Not0tccSIwn5iwVRlDi45?$BtW');
 
 export async function generateStaticParams() {
-    await db.admins.authWithPassword('abelvanhulst@gmail.com', '7g#Z5iVbQ&yTd8Not0tccSIwn5iwVRlDi45?$BtW');
     const collection = await db.collection('latijnse_woorden').getFullList(200 /* batch size */, {
     });
 
@@ -27,8 +27,22 @@ export async function generateStaticParams() {
 //     return record1
 // }
 
+async function verwanden(woord) {
+    await Promise.all(woord.andere_vormen.map(async (verbandje) => {
+        const verband = await db.collection('latijnse_woorden').getOne(verbandje, {
+            expand: 'relField1,relField2.subRelField',
+        });
+        return <Link key={verband.id} href={"/woorden/" + verband.id + "/"}><h1>{verband.latijn}</h1></Link>
+    }))
+}
+
+function zijnErVerwanden(woord) {
+    if (woord.andere_vormen.length != 0) {
+        return verwanden(woord)
+    }
+}
+
 export default async function Woord({ params }) {
-    await db.admins.authWithPassword('abelvanhulst@gmail.com', '7g#Z5iVbQ&yTd8Not0tccSIwn5iwVRlDi45?$BtW');
     const woord = await db.collection('latijnse_woorden').getOne(params.id, {
         expand: 'relField1,relField2.subRelField',
     });
@@ -38,12 +52,7 @@ export default async function Woord({ params }) {
             <h1>{woord.latijn}</h1>
             <h1>{woord.latijn} betekent {woord.nederlands}</h1>
             <h1>Verwanden zijn:</h1>
-            {await Promise.all(woord.andere_vormen.map(async (verbandje) => {
-                const verband = await db.collection('latijnse_woorden').getOne(verbandje, {
-                    expand: 'relField1,relField2.subRelField',
-                });
-                return <Link key={verband.id} href={"/woorden/" + verband.id + "/"}><h1>{verband.latijn}</h1></Link>
-            }))}
+            {zijnErVerwanden(woord)}
         </>
     )
 }
