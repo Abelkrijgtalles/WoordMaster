f70805a55ee0fd27cf9acb07872bd2f40e9bd847
Abel <abelvanhulst@gmail.com>
2023-10-11 22:59:54 +0200

    update leren

--- app/Footer.tsx
+++ app/Footer.tsx
@@ -3,8 +3,8 @@ import Link from "next/link";
 export default function Footer() {
     return (
         <>
-            <Link href="https://github.com/Abelkrijgtalles/minerva-online">GitHub</Link>
-            <p>Alles is gelicenseerd onder de <Link href=""></Link> License.</p>        
+        <Link href="https://github.com/Abelkrijgtalles/minerva-online">GitHub</Link>
+        <p>Alles is gelicenseerd met <Link href="https://raw.githubusercontent.com/Abelkrijgtalles/minerva-online/main/LICENSE">de GNU GPL v3 License</Link>.</p>
         </>
     )
 }
--- app/leren/page.tsx
+++ app/leren/page.tsx
@@ -1,26 +1,9 @@
-import Link from 'next/link';
-import PocketBase from 'pocketbase';
-
-export const revalidate = 60
-
-const db = new PocketBase(process.env.POCKETBASE_SERVER)
-
-async function getDingen() {
-    await db.admins.authWithPassword(process.env.POCKETBASE_ADMIN_EMAIL, process.env.POCKETBASE_ADMIN_PASSWORD);
-    const collection = await db.collection('woorden_te_leer').getFullList(200 /* batch size */, {
-        sort: '-hoofdstuk,-na_hoofdstuk'
-    });
-    db.authStore.clear();
-    return collection.reverse()
-}
-
-export default async function Leren() {
-    const dingen = await getDingen()
+export default function Leren() {
+    
     return (
-        <><h1>hi</h1>
-            {dingen.map((ding) => {
-                return <Link key={ding.id} href={'/leren/' + ding.id + "/"}><h1>{ding.hoofdstuk}{ding.na_hoofdstuk}</h1></Link>
-            })}
-        </>
+        
+        <h1>Nu ff niet oké, ik ben aan iets aan het werken</h1>
+        
     )
-}
+    
+};
