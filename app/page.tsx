3afda41a3d5c36a06b73ae7da47c532191f11bd5
Abel <abelvanhulst@gmail.com>
2024-11-23 00:33:12 +0100

    idk

--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,11 @@ import {ReactTyped} from "react-typed";
 
 export default function Home() {
     return (
-        <>
-            <div className="text-center text-7xl">
+        <div className="text-center">
+            <div className="text-9xl text-wrap mb-20">
                 <p>Word een echte</p>
                 <ReactTyped
-                    className="bg-dark_slate_gray rounded-3xl"
+                    className="bg-dark_slate_gray rounded-3xl text-wrap break-words"
                     strings={[
                         "WoordMaster.",
                         "WordMaster.",
@@ -24,6 +24,8 @@ export default function Home() {
                     loop
                 />
             </div>
-        </>
+            <h1 className="mb-2">Dé manier om woorden te leren.</h1>
+            <p className="text-2xl">Waarom niet nu? Wat stopt je? Leer nu gewoon je woorden!</p>
+        </div>
     )
 }
